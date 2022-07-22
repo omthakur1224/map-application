c64b4cf1a5eb6522ce8c2726234e6bc3dc701737
omthakur1224 <omthakur1224@gmail.com>
2022-07-22 22:32:02 +0530

    added sidebar

--- main.js
+++ main.js
@@ -1,27 +1,7 @@
+// const stores=require('./store')
+import {stores} from './store'
+
 mapboxgl.accessToken = 'pk.eyJ1Ijoib210aGFrdXIxMjI0IiwiYSI6ImNsNXc3azh2MDBiMXozaW56dzZub3Y5MnUifQ.12VimV4yzVRdBB0K3S919g';
-
-// navigator.geolocation.getCurrentPosition(successLocation,errorLocation,
-//     {enableHighAccuracy:true})
-
-//     function successLocation(position){
-//         console.log(position)
-//         setMap([position.coords.longitude, position.coords.latitude])
-//     }
-
-//     function errorLocation(){
-//         //when location is not available it will take to menchester location
-//         setMap([-2.24, 53.48])
-//     }
-
-//     function setMap(center){
-    
-//     var map = new mapboxgl.Map({
-//       container: 'map',
-//       style: 'mapbox://styles/mapbox/streets-v11',
-//       center:center,
-//       zoom:12
-//     });
-// map.addControl(new mapboxgl.NavigationControl());
 
 var map = new mapboxgl.Map({
     container: 'map',
@@ -215,14 +195,235 @@ map.on('load', () => {
     });
     });
 
+    /* Assign a unique ID to each store */
+stores.features.forEach(function (store, i) {
+    store.properties.id = i;
+  });
+
+  // //building store
+  // function buildLocationList(stores) {
+  //     for (const store of stores.features) {
+  //       /* Add a new listing section to the sidebar. */
+  //       const listings = document.getElementById('listings');
+  //       const listing = listings.appendChild(document.createElement('div'));
+  //       /* Assign a unique `id` to the listing. */
+  //       listing.id = `listing-${store.properties.id}`;
+  //       /* Assign the `item` class to each listing for styling. */
+  //       listing.className = 'item';
+    
+  //       /* Add the link to the individual listing created above. */
+  //       const link = listing.appendChild(document.createElement('a'));
+  //       link.href = '#';
+  //       link.className = 'title';
+  //       link.id = `link-${store.properties.id}`;
+  //       link.innerHTML = `${store.properties.address}`;
+    
+  //       /* Add details to the individual listing. */
+  //       const details = listing.appendChild(document.createElement('div'));
+  //       details.innerHTML = `${store.properties.city}`;
+  //       if (store.properties.phone) {
+  //         details.innerHTML += ` · ${store.properties.phoneFormatted}`;
+  //       }
+  //       if (store.properties.distance) {
+  //         const roundedDistance = Math.round(store.properties.distance * 100) / 100;
+  //         details.innerHTML += `<div><strong>${roundedDistance} miles away</strong></div>`;
+  //       }
+  //     }
+  //   }
+
+  map.on('load', () => {
+    /* Add the data to your map as a layer */
+    map.addLayer({
+      id: 'locations',
+      type: 'circle',
+      /* Add a GeoJSON source containing place coordinates and information. */
+      source: {
+        type: 'geojson',
+        data: stores
+      }
+    });
+    buildLocationList(stores);
+  });
+
+
+ 
+/**
+* Assign a unique id to each store. You'll use this `id`
+* later to associate each point on the map with a listing
+* in the sidebar.
+*/
+stores.features.forEach((store, i) => {
+  store.properties.id = i;
+  });
+   
+  /**
+  * Wait until the map loads to make changes to the map.
+  // */
+  map.on('load', () => {
+  /**
+  * This is where your '.addLayer()' used to be, instead
+  * add only the source without styling a layer
+  */
+  map.addSource('places', {
+  'type': 'geojson',
+  'data': stores
+  });
+   
+  /**
+  * Add all the things to the page:
+  * - The location listings on the side of the page
+  * - The markers onto the map
+  */
+  buildLocationList(stores);
+  addMarkers();
+  });
+   
+  /**
+  * Add a marker to the map for every store listing.
+  **/
+  function addMarkers() {
+  /* For each feature in the GeoJSON object above: */
+  for (const marker of stores.features) {
+  /* Create a div element for the marker. */
+  const el = document.createElement('div');
+  /* Assign a unique `id` to the marker. */
+  el.id = `marker-${marker.properties.id}`;
+  /* Assign the `marker` class to each marker for styling. */
+  el.className = 'marker';
+   
+  /**
+  * Create a marker using the div element
+  * defined above and add it to the map.
+  **/
+  new mapboxgl.Marker(el, { offset: [0, -23] })
+  .setLngLat(marker.geometry.coordinates)
+  .addTo(map);
+   
+  /**
+  * Listen to the element and when it is clicked, do three things:
+  * 1. Fly to the point
+  * 2. Close all other popups and display popup for clicked store
+  * 3. Highlight listing in sidebar (and remove highlight for all other listings)
+  **/
+  el.addEventListener('click', (e) => {
+  /* Fly to the point */
+  flyToStore(marker);
+  /* Close all other popups and display popup for clicked store */
+  createPopUp(marker);
+  /* Highlight listing in sidebar */
+  const activeItem = document.getElementsByClassName('active');
+  e.stopPropagation();
+  if (activeItem[0]) {
+  activeItem[0].classList.remove('active');
+  }
+  const listing = document.getElementById(
+  `listing-${marker.properties.id}`
+  );
+  listing.classList.add('active');
+  });
+  }
+  }
+   
+  /**
+  * Add a listing for each store to the sidebar.
+  **/
+  function buildLocationList(stores) {
+  for (const store of stores.features) {
+  /* Add a new listing section to the sidebar. */
+  const listings = document.getElementById('listings');
+  const listing = listings.appendChild(document.createElement('div'));
+  /* Assign a unique `id` to the listing. */
+  listing.id = `listing-${store.properties.id}`;
+  /* Assign the `item` class to each listing for styling. */
+  listing.className = 'item';
+   
+  /* Add the link to the individual listing created above. */
+  const link = listing.appendChild(document.createElement('a'));
+  link.href = '#';
+  link.className = 'title';
+  link.id = `link-${store.properties.id}`;
+  link.innerHTML = `${store.properties.address}`;
+   
+  /* Add details to the individual listing. */
+  const details = listing.appendChild(document.createElement('div'));
+  details.innerHTML = `${store.properties.city}`;
+  if (store.properties.phone) {
+  details.innerHTML += ` &middot; ${store.properties.phoneFormatted}`;
+  }
+   
+  // /**
+  // * Listen to the element and when it is clicked, do four things:
+  // * 1. Update the `currentFeature` to the store associated with the clicked link
+  // * 2. Fly to the point
+  // * 3. Close all other popups and display popup for clicked store
+  // * 4. Highlight listing in sidebar (and remove highlight for all other listings)
+  // **/
+  link.addEventListener('click', function () {
+  for (const feature of stores.features) {
+  if (this.id === `link-${feature.properties.id}`) {
+  flyToStore(feature);
+  createPopUp(feature);
+  }
+  }
+  const activeItem = document.getElementsByClassName('active');
+  if (activeItem[0]) {
+  activeItem[0].classList.remove('active');
+  }
+  this.parentNode.classList.add('active');
+  });
+  }
+  }
+   
+  /**
+  * Use Mapbox GL JS's `flyTo` to move the camera smoothly
+  * a given center point.
+  **/
+  function flyToStore(currentFeature) {
+  map.flyTo({
+  center: currentFeature.geometry.coordinates,
+  zoom: 15
+  });
+  }
+   
+  /**
+  * Create a Mapbox GL JS `Popup`.
+  **/
+  function createPopUp(currentFeature) {
+  const popUps = document.getElementsByClassName('mapboxgl-popup');
+  if (popUps[0]) popUps[0].remove();
+  const popup = new mapboxgl.Popup({ closeOnClick: false })
+  .setLngLat(currentFeature.geometry.coordinates)
+  .setHTML(
+  `<h3>Sweetgreen</h3><h4>${currentFeature.properties.address}</h4>`
+  )
+  .addTo(map);
+  }
 
 
 
 
+// navigator.geolocation.getCurrentPosition(successLocation,errorLocation,
+//     {enableHighAccuracy:true})
 
+//     function successLocation(position){
+//         console.log(position)
+//         setMap([position.coords.longitude, position.coords.latitude])
+//     }
 
+//     function errorLocation(){
+//         //when location is not available it will take to menchester location
+//         setMap([-2.24, 53.48])
+//     }
 
-
+//     function setMap(center){
+    
+//     var map = new mapboxgl.Map({
+//       container: 'map',
+//       style: 'mapbox://styles/mapbox/streets-v11',
+//       center:center,
+//       zoom:12
+//     });
+// map.addControl(new mapboxgl.NavigationControl());
 
 
 
